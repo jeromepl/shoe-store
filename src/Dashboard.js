22f616f436b609d10fbc6e6db4d8b66efe3a48c8
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2021-01-12 18:05:26 -0500

    Add dashboard table design

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,20 +1,65 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { values } from 'mobx';
+import styled from 'styled-components';
 
-import dataStore from './store';
+import dataStore, { STORE_LIST, SHOE_LIST } from './store';
 
 
-const Dashboard = observer(() => {
-    return <div className="Dashboard">
-        {values(dataStore.stores).map((store, i) => (
-            <div key={i} >
-                { values(store['inventory']).map((shoe, j) => (
-                    <span key={j}>{shoe > 0 ? shoe : '-'}</span>
-                ))}
-            </div>
-        ))}
-    </div>;
+const Grid = styled.table`
+    border-spacing: 1px;
+
+    & tr {
+        height: 40px;
+    }
+
+    & td:not(.row-header) {
+        width: 40px;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    & td.row-header {
+        font-weight: bold;
+        text-align: right;
+    }
+
+`;
+
+const CellStyled = styled.td`
+    background-color: ${props => props.color ?? 'white'};
+    transition: background-color ease 0.5s;
+`;
+
+const Cell = observer((props) => {
+    const inventory = dataStore.stores.get(props.store).inventory.get(props.model);
+    const color = inventory >= 0 ? `hsl(${(inventory / 100) * 156}deg 55% 51%)` : 'white';
+    return <CellStyled color={color}>
+        {inventory >= 0 ? inventory : '-'}
+    </CellStyled>;
 });
+
+
+const Dashboard = () => {
+    return <div className="Dashboard">
+        <Grid>
+            <tbody>
+                <tr>
+                    <th></th>
+                    {SHOE_LIST.map((model) =>
+                        <th key={model} title={model}>{model.slice(0, 3)}.</th>
+                    )}
+                </tr>
+                {STORE_LIST.map((store) =>
+                    <tr key={store}>
+                        <td className='row-header'>{store}</td>
+                        {SHOE_LIST.map((model) =>
+                            <Cell key={store + model} store={store} model={model} />
+                        )}
+                    </tr>
+                )}
+            </tbody>
+        </Grid>
+    </div>;
+};
 
 export default Dashboard;
