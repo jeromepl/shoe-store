ecae3f41b82773f441747b9c23fc2f01bc25aea5
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2021-01-12 16:50:02 -0500

    Add header design

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import useHover from './useHover';
 import logo from './offline_shopping.svg';
 
 
+const Title = styled.h1`
+    display: inline-block;
+    margin-left: 30px;
+    font-size: 36px;
+    color: black;
+    cursor: pointer;
+
+    transition: color ease 1s;
+
+    &.open {
+        color: white;
+    }
+`;
+
+const LogoContainer = styled.div`
+    position: absolute;
+    left: -159px;
+    top: -99px;
+    width: 0px;
+    height: 0px;
+    border-radius: 50%;
+    z-index: -10;
+    overflow: hidden;
+
+    transition: width ease 1s, height ease 1s;
+
+    &.open {
+        width: 700px;
+        height: 700px;
+    }
+`;
+
+const Logo = styled.img`
+    width: 700px;
+    filter: brightness(0.9);
+`;
+
 const Header = () => {
+    const [hoverRef, isHovered] = useHover();
+
     return <header className="Header">
-        <h1>ALDO</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <Title ref={hoverRef} className={isHovered ? 'open' : ''}>ALDO</Title>
+        <LogoContainer className={isHovered ? 'open' : ''}>
+            <Logo src={logo} className={`App-logo ${isHovered ? 'open' : ''}`} alt="logo" />
+        </LogoContainer>
     </header>;
 };
 
